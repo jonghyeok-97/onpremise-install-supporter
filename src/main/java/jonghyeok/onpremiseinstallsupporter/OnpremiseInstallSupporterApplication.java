267ea6c6ef2f93cca5c13f4bc06dafd0f1b7ef6f
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-10-20 22:49:13 +0900

    프로젝트 초기 커밋

---
+++ src/main/java/jonghyeok/onpremiseinstallsupporter/OnpremiseInstallSupporterApplication.java
@@ -0,0 +1,13 @@
+package jonghyeok.onpremiseinstallsupporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnpremiseInstallSupporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnpremiseInstallSupporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jonghyeok/onpremiseinstallsupporter/OnpremiseInstallSupporterApplicationTests.java
@@ -0,0 +1,13 @@
+package jonghyeok.onpremiseinstallsupporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnpremiseInstallSupporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
